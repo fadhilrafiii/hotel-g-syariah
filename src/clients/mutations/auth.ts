bebb8d40cb3914c2a2c4c816b3d21c50ac3d148f
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2023-01-29 15:55:42 +0700

    Add vercel config for routing failed in vercel

--- src/clients/mutations/auth.ts
+++ src/clients/mutations/auth.ts
@@ -4,6 +4,7 @@ export const GQL_MUTATION_LOGIN_USER = gql`
   mutation c($username: String!, $password: String!) {
     login(input: { username: $username, password: $password }) {
       accessToken
+      user
     }
   }
 `;
