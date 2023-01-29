f421159e0bdccbaf5b3e31b4ef1eb02d78c6bad9
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2023-01-29 16:32:31 +0700

    fix login response payload

--- src/clients/mutations/auth.ts
+++ src/clients/mutations/auth.ts
@@ -4,7 +4,10 @@ export const GQL_MUTATION_LOGIN_USER = gql`
   mutation c($username: String!, $password: String!) {
     login(input: { username: $username, password: $password }) {
       accessToken
-      user
+      user {
+        username
+        role
+      }
     }
   }
 `;
