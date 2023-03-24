bb64f2ca7dd970b6b072c83fc8998eb9268aac38
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2023-03-25 02:33:41 +0700

    Create TimeDropdown component

---
+++ src/components/TimeDropdown/index.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, useMemo } from "react";
+import dayjs from "src/shared/utils/dayjs";
+import Button from "../Button";
+import CalendarCheckOutlinedIcon from "../icons/CalendarCheckOutlinedIcon";
+import ChevronDownIcon from "../icons/ChevronDownIcon";
+
+import "./TimeDropdown.scss";
+
+const TIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
+const NOW = dayjs().endOf("day").format(TIME_FORMAT);
+
+const options = [
+  {
+    label: "Hari ini",
+    value: `${dayjs().startOf("day").format(TIME_FORMAT)}-${NOW}`,
+  },
+  {
+    label: "7 Hari Terakhir",
+    value: `${dayjs()
+      .subtract(1, "week")
+      .startOf("day")
+      .format(TIME_FORMAT)}-${NOW}`,
+  },
+  {
+    label: "1 Bulan Lalu",
+    value: `${dayjs()
+      .subtract(1, "month")
+      .startOf("day")
+      .format(TIME_FORMAT)}-${NOW}`,
+  },
+  {
+    label: "12 Bulan Lalu",
+    value: `${dayjs()
+      .subtract(1, "year")
+      .startOf("day")
+      .format(TIME_FORMAT)}-${NOW}`,
+  },
+];
+
+interface TimeDropdownProps {
+  containerClass?: string;
+  onChange: (value: string) => void;
+}
+
+const TimeDropdown = ({ containerClass = "", onChange }: TimeDropdownProps) => {
+  const [selectedTime, setSelectedTime] = useState(options[0].value);
+  const [shouldOpenDropdown, setShouldOpenDropdown] = useState(false);
+
+  useEffect(() => {
+    onChange(selectedTime);
+  }, [selectedTime]);
+
+  const selectedLabel = useMemo(
+    () => options.find((opt) => opt.value === selectedTime)?.label,
+    [selectedTime]
+  );
+
+  return (
+    <div
+      className={`container ${containerClass} relative`}
+      tabIndex={0}
+      onBlur={() => setShouldOpenDropdown(false)}
+    >
+      <Button
+        className="w-full"
+        variant={Button.Variant.Filled}
+        theme={Button.Theme.Secondary}
+        startIcon={<CalendarCheckOutlinedIcon size={20} />}
+        endIcon={<ChevronDownIcon size={8} />}
+        onClick={() => setShouldOpenDropdown((prev) => !prev)}
+      >
+        {selectedLabel}
+      </Button>
+      {shouldOpenDropdown && (
+        <div className="options absolute top-[calc(100%+8px)] py-3 rounded-md w-full bg-white">
+          {options.map((opt, key) => {
+            const isSelected = opt.value === selectedTime;
+
+            return (
+              <div
+                key={key}
+                role="button"
+                onClick={() => setSelectedTime(opt.value)}
+                className="flex items-center px-3 py-1 gap-x-[6px]"
+              >
+                <span className={`${isSelected ? "opacity-100" : "opacity-0"}`}>
+                  &#10004;
+                </span>
+                <span className={`${isSelected ? "font-semibold" : ""}`}>
+                  {opt.label}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TimeDropdown;
