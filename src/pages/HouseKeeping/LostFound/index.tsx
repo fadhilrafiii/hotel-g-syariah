56516794cfd5f32aeb6267aff66aad1340a4b022
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-03-26 23:15:32 +0700

    Fix modal and button style

--- src/pages/HouseKeeping/LostFound/index.tsx
+++ src/pages/HouseKeeping/LostFound/index.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
-import Modal from "src/components/Modal";
+import { useState } from "react";
 import HKLostFoundTable from "./LostFoundTable";
 import ModalLostFound from "./AddLostFoundModal";
 
 const HouseKeepingLostFoundPage = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
     <div className="flex flex-col gap-6 p-2">
       <div className="flex justify-between items-center">
         <h3 className="font-semibold text-2xl">
           Lost & Found <span className="text-base font-normal">{`(67)`}</span>
         </h3>
-        <ModalLostFound />
+        <button
+          className="rounded-md font-semibold bg-blue-600 text-white px-3 py-[6px]"
+          onClick={() => setIsOpenModal(true)}
+        >
+          + Tambah Barang Baru
+        </button>
       </div>
       <div className="flex gap-2">
         <div className="text-sm font-medium bg-blue-600 rounded-3xl text-white py-1 px-3 cursor-pointer">
@@ -24,6 +30,12 @@ const HouseKeepingLostFoundPage = () => {
         </div>
       </div>
       <HKLostFoundTable />
+
+      {/* Modal */}
+      <ModalLostFound
+        isOpen={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+      />
     </div>
   );
 };
--- src/pages/HouseKeeping/StatusKamar/index.tsx
+++ src/pages/HouseKeeping/StatusKamar/index.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import StatusHouseKeeping from "../Dashboard/StatusHouseKeeping";
 import ModalStatusKamar from "./AddNewTaskModal";
 import HKRoomTable from "./HKRoomTable";
 import HKRoomType from "./HKRoomType";
 
 const HouseKeepingKamarPage = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
     <div className="flex flex-col gap-6 p-2">
       <div className="flex justify-between items-center">
         <StatusHouseKeeping />
-        <ModalStatusKamar />
+        <button
+          className="rounded-md font-semibold bg-blue-600 text-white px-3 py-[6px]"
+          onClick={() => setIsOpenModal(true)}
+        >
+          + Penugasan Baru
+        </button>
       </div>
       <HKRoomType />
       <HKRoomTable />
+
+      {/* Modal */}
+      <ModalStatusKamar
+        isOpen={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+      />
     </div>
   );
 };
